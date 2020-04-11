26dd320276c1e84f3ba5a6d95bb5a526cbb58837
FabianSchuetze <fabian.schutze@eui.eu>
2020-04-11 12:00:12 +0200

    Pennfudan runs

--- examples/learner.py
+++ examples/learner.py
@@ -19,9 +19,9 @@ def train_test_set():
     dataset = torch.utils.data.Subset(dataset, indices[:-50])
     dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
     data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=1, shuffle=True, num_workers=0)
+        dataset, batch_size=1, shuffle=True, num_workers=4)
     data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=1, shuffle=False, num_workers=0)
+        dataset_test, batch_size=1, shuffle=False, num_workers=4)
     return data_loader, data_loader_test
 
 def get_transform(train):
